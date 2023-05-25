ec8a066f1cc58ea9106759685f374ebd4dc7e1e7
Ivan Zlatov <ivanzlatoff@gmail.com>
2023-05-26 00:04:40 +0100

    changing origin cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'http://localhost:3001']
+    // origin: ['http://localhost:3000', 'http://localhost:3001']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
