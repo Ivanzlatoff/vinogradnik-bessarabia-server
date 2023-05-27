6b55a8442e637057e6fe983d1d69104322c6208e
Ivan Zlatov <ivanzlatoff@gmail.com>
2023-05-27 20:17:55 +0100

    Remocing commented out code

--- index.js
+++ index.js
@@ -23,16 +23,6 @@ mongoose.connect(process.env.MONGO_URL)
         console.log(err);
     });
 
-// app.use(cors({
-//     AccessControlAllowOrigin: '*',
-//     credentials: true,
-//     origin: [
-//         'http://localhost:3000', 
-//         'http://localhost:3001', 
-//         'https://646feb347dd133147b9208b7--comforting-ganache-bb49bc.netlify.app/',
-//         'https://main.dri9uy0bmhz14.amplifyapp.com/'
-//     ]
-// }));
 app.use(cors({
   accessControlAllowOrigin: [
     'http://localhost:3000',
